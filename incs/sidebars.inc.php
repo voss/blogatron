384fa93e397c9938b6bdc984c25eacd187c8c496
Jonas Voss <jonas.voss@gmail.com>
2007-07-02 18:58:09 +0000

    added last.fm tracks, and del.icio.us link from sidebar

--- incs/sidebars.inc.php
+++ incs/sidebars.inc.php
@@ -2,16 +2,14 @@
     <p class="bio"><img src="/img/buddy_ikon_cropped_100x100.png" style="float:left;
 border: 1px solid #555; padding: 3px; margin-right: 5px;" alt="Bio
 picture, taken by Rasmus Rasmussen" title="Me, through the lens of
-Rasmus Rasmussen" />verture.net
-is the personal website of me, Jonas Voss, and this is my blog. I live
-in Dublin, Ireland, where I work for <a
-href="http://google.ie/">Google</a>, but was born and bred in Copenhagen, Denmark. I write
-about anything that comes to mind. Really.</p>
+Rasmus Rasmussen" />verture.net is the personal website of me, Jonas Voss, and this is my blog. I've lived in Dublin, Ireland since 2005, where I work for <a
+href="http://google.ie/">Google</a>. I was born and bred in Copenhagen, Denmark. I write about anything that comes to mind. Really.</p>
 			<ul style="padding-top:0;margin-top:0;">
-				<li><img src="/img/rsssmall.png" alt="RSS ikon" style="vertical-align: middle; padding-right: 3px; border:0"/><a href="http://feeds.feedburner.com/verture" title="RSS-feed af indlæg">Entries</a></li>
-				<li><img src="/img/rsssmall.png" alt="RSS ikon" style="vertical-align: middle; padding-right: 3px; border:0"/><a href="http://feeds.feedburner.com/verturekommentarer" title="RSS-feed af kommentarer">Comments</a></li>
-        <li><img src="/img/23hq-favicon.png" alt="Images at 23" style="vertical-align: middle; padding-right: 3px; border:0" /><a href="http://23hq.com/voss">Images at 23</a></li>
-        <li><img src="/img/flickr_favicon.png" alt="Images at flickr" style="vertical-align: middle; padding-right: 3px; border:0" /><a href="http://www.flickr.com/photos/voss/">Images at flickr</a></li>
+				<li><img src="/img/rsssmall.png" alt="RSS ikon" class="sidebaricon" /><a href="http://feeds.feedburner.com/verture" title="RSS-feed af indlæg">Entries</a></li>
+				<li><img src="/img/rsssmall.png" alt="RSS ikon" class="sidebaricon" /><a href="http://feeds.feedburner.com/verturekommentarer" title="RSS-feed af kommentarer">Comments</a></li>
+        <li><img src="/img/23hq-favicon.png" alt="Images at 23" class="sidebaricon" /><a href="http://23hq.com/voss">Images at 23</a></li>
+        <li><img src="/img/flickr_favicon.png" alt="Images at flickr" class="sidebaricon" /><a href="http://www.flickr.com/photos/voss/">Images at flickr</a></li>
+		<li><img src="/img/del.icio.us-favicon.png" alt="del.icio.us logo" class="sidebaricon" /><a href="http://del.icio.us/voss">My del.icio.us</a></li>
 			<li id="search">
 				<?php @include('incs/menu.inc');?>
 			</li>
@@ -25,7 +23,11 @@ about anything that comes to mind. Really.</p>
 		</div>
 		<div id="readerunit">
 			<script type="text/javascript" src="http://www.google.com/reader/ui/publisher.js"></script>
-<script type="text/javascript" src="http://www.google.com/reader/public/javascript/user/17963144626664830161/label/sidedish?n=10&amp;callback=GRC_p(%7Bc%3A'-'%2Ct%3A'Sidedish'%2Cs%3A'true'%7D)%3Bnew%20GRC"></script>
+<script type="text/javascript" src="http://www.google.com/reader/public/javascript/user/17963144626664830161/label/sidedish?n=5&amp;callback=GRC_p(%7Bc%3A'-'%2Ct%3A'Sidedish'%2Cs%3A'true'%7D)%3Bnew%20GRC"></script>
+		</div>
+		<div id="readerunit">
+			<script type="text/javascript" src="http://www.google.com/reader/ui/publisher.js"></script>
+			<script type="text/javascript" src="http://www.google.com/reader/public/javascript/user/17963144626664830161/label/recenttracks?n=5&callback=GRC_p(%7Bc%3A%22-%22%2Ct%3A%22Recent%20tracks%22%2Cs%3A%22false%22%7D)%3Bnew%20GRC"></script>
 		</div>
 
 		<div class="unit">
