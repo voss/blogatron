863c8955bc93726f0d6c21c9617b946411fafb3a
Jonas Voss <jonas.voss@gmail.com>
2007-11-05 15:35:41 +0000

    changed some data presentation on /dl/stats.php

--- dl/stats.php
+++ dl/stats.php
@@ -12,17 +12,18 @@ include('/home/voss/blog.verture.net/incs/db.inc.php');
 # Setting timezone:
 putenv("TZ=Europe/Dublin");
 
-$stat_sql = "SELECT * from dls order by timestamp asc";
+$stat_sql = "SELECT * from dls order by timestamp desc";
 $result = mysql_query($stat_sql) or print mysql_error();
 $numrows = mysql_num_rows($result);
 ?>
+<p><?=$numrows;?> har nedlastet filen</p>
 <table border="1">
 <th>IP</th><th>filnavn</th><th>klokkeslet</th>
 <?php
 while($row = mysql_fetch_array($result)) {
 	extract($row);
 	$date = date('r', $timestamp);
-  if($filename == "23-photos.zip")
+  if(preg_match("/23-photos.zip|songsforyou.zip/",$filename))
   {
     print "<tr style=\"font-family:monospace; color: white; background:red;\"><td>{$ip}</td><td>{$filename}</td><td>{$date}</td></tr>";
   }
@@ -33,7 +34,6 @@ while($row = mysql_fetch_array($result)) {
 }
 ?>
 </table>
-<p><?=$numrows;?> har nedlastet filen</p>
 </body>
 </html>
 <?php
