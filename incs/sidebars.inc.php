a1527d4429d1cf5dcd2c27e65318391c63c024d1
Jonas Voss <jonas.voss@gmail.com>
2008-09-30 21:17:57 +0000

    Changing muxtape link to point to my opentape

--- incs/sidebars.inc.php
+++ incs/sidebars.inc.php
@@ -37,7 +37,7 @@ document.write("<a href='mailto:"+link+"'>jonas at verture dot net</a>")
 			    <li><img src="/img/del.icio.us-favicon.png" alt="del.icio.us logo" class="sidebaricon" /><a href="/io/http://del.icio.us/voss">del.icio.us braindump</a></li>
 		        <li><img src="/img/last.fm_favicon.png" alt="last.fm logo" class="sidebaricon" /><a href="/io/http://last.fm/user/bobbybonzai">Music at last.fm</a></li>
 		        <li><img src="/img/tumblr.gif" alt="tumblr logo" class="sidebaricon" /><a href="/io/http://inc.verture.net/">tumblr</a></li>
-				<li><img src="/img/muxtape.png" alt="Muxtape Logo" class="sidebaricon" /><a href="/io/http://voss.muxtape.com/">my muxtape</a></li>
+				<li><img src="/img/muxtape.png" alt="Muxtape Logo" class="sidebaricon" /><a href="/io/http://computerlove.ie/tape/">my OpenTape</a></li>
         	</ul>
 		</div>
 		<div class="unit">
